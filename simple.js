968066bd69ff3d78b2b5950e89a4fff97c998de5
Martinsperka1 <41669350+Martinsperka1@users.noreply.github.com>
2018-07-27 14:38:56 +0200

    Create simple.js

---
+++ simple.js
@@ -0,0 +1,15 @@
+let name = "peter";
+var age = 23;
+const nationality = "slovak";
+console.log(name);
+name = "robert";
+
+console.log(name);
+
+function pow(val) {
+    return val * val;
+
+}
+let num = pow(3);
+
+console.log(num);
