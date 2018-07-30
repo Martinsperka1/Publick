5c2e7a8e0bec94e48b2c2897b9f33ac375805615
Martinsperka1 <41669350+Martinsperka1@users.noreply.github.com>
2018-07-30 10:23:53 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+/* A sequence of characters in the code that identifies a variable, 
+function, or property. In JavaScript, identifiers can contain only 
+alphanumeric characters (or "$" or "_"), and may not start with a digit. 
+An identifier differs from a string in that a string is data, while an 
+identifier is part of the code.  */
+
+let name = 'Peter';
+console.log(name);
+
+name = 'Lucia';
+console.log(name);
+
+let x = 10, y = 20;
+console.log(`x:${x}, y:${y}`);
+
+let a = b = c = d = 11;
+console.log(`a:${a}, b:${c}, c:${c}, d:${d}`);
